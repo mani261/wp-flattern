80f2caaf391f6ebe1ab67974d5b7e62253e49365
MaNi <mani.razavizadeh@gmail.com>
2021-01-15 19:39:42 +0330

    Create main structure

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+function wpflattern_files()
+{
+
+    wp_enqueue_script( 'bootstrap', get_theme_file_uri( 'assets/vendor/bootstrap/js/bootstrap.bundle.min.js', array('jquery'), NULL, true ) );
+    wp_enqueue_script( 'jquery_easing', get_theme_file_uri( 'assets/vendor/jquery.easing/jquery.easing.min.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'mail_validate_file', get_theme_file_uri( 'assets/vendor/php-email-form/validate.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'jquery_sticky', get_theme_file_uri( 'assets/vendor/jquery-sticky/jquery.sticky.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'isotype', get_theme_file_uri( 'assets/vendor/isotope-layout/isotope.pkgd.min.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'venobox', get_theme_file_uri( 'assets/vendor/venobox/venobox.min.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'waypoints', get_theme_file_uri( 'assets/vendor/waypoints/jquery.waypoints.min.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'owl_carousel', get_theme_file_uri( 'assets/vendor/owl.carousel/owl.carousel.min.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'aos', get_theme_file_uri( 'assets/vendor/aos/aos.js' ), NULL, NULL, true );
+    wp_enqueue_script( 'template_main_file', get_theme_file_uri( 'assets/js/main.js' ), NULL, '1.0', true );
+
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i' );
+    wp_enqueue_style( 'vendor_file1', get_theme_file_uri('assets/vendor/bootstrap/css/bootstrap.min.css') );
+    wp_enqueue_style( 'vendor_file2', get_theme_file_uri('assets/vendor/icofont/icofont.min.css') );
+    wp_enqueue_style( 'vendor_file3', get_theme_file_uri('assets/vendor/boxicons/css/boxicons.min.css') );
+    wp_enqueue_style( 'vendor_file4', get_theme_file_uri('assets/vendor/animate.css/animate.min.css') );
+    wp_enqueue_style( 'vendor_file5', get_theme_file_uri('assets/vendor/venobox/venobox.css') );
+    wp_enqueue_style( 'vendor_file6', get_theme_file_uri('assets/vendor/owl.carousel/assets/owl.carousel.min.css') );
+    wp_enqueue_style( 'vendor_file7', get_theme_file_uri('assets/vendor/aos/aos.css') );
+    wp_enqueue_style( 'template_file', get_theme_file_uri('assets/css/style.css') );
+    wp_enqueue_style( 'flattern_main_styles', get_stylesheet_uri() );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'wpflattern_files' );
+
+function wpflattern_features() {
+    add_theme_support( 'title-tag' );
+}
+
+add_action( 'after_setup_theme', 'wpflattern_features' );
---
+++ header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ) ?>">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(  ) ?>>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i class="icofont-envelope"></i><a href="mailto:contact@example.com">contact@example.com</a>
+        <i class="icofont-phone"></i> +1 5589 55488 55
+      </div>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="#" class="skype"><i class="icofont-skype"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html">Flattern</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="<?php echo get_theme_file_uri(); ?>/assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a href="testimonials.html">Testimonials</a></li>
+          <li><a href="pricing.html">Pricing</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.html">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
